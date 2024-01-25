fb61f37f47b0a5e778e6c9cd96a0dce0672ab591
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-01-25 16:28:34 +0700

    Modify database and code.

--- connect_sqlite3/connect_sqlite3.py
+++ connect_sqlite3/connect_sqlite3.py
@@ -2,21 +2,22 @@ import sqlite3
 from prettytable import from_db_cursor
 from pathlib import Path
 
-# get the current working directory of the script
-database_file = str(Path(__file__).parent) + '\coffee_shop.sqlite3'
+if __name__ == '__main__':
+    # get the current working directory of the script
+    database_file = str(Path(__file__).parent) + '\coffee_shop.sqlite3'
 
-connection = sqlite3.connect(database_file)
-cursor = connection.cursor() # a database cursor object
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor() # a database cursor object
 
-sql = '''SELECT customer_id, customer_name, major
-FROM customers
-WHERE major <> ''
-ORDER BY major, customer_name
-'''
-cursor.execute(sql)
+    sql = '''SELECT customer_id, customer_name, major
+    FROM customers
+    WHERE major = 'Vaccinologist'
+    ORDER BY customer_name
+    '''
+    cursor.execute(sql)
 
-table = from_db_cursor(cursor)
+    table = from_db_cursor(cursor)
 
-table.align['customer_name'] = 'l' # align left
-table.align['major'] = 'r' # align right
-print(table)
+    table.align['customer_name'] = 'l' # align left
+    table.align['major'] = 'r' # align right
+    print(table)
